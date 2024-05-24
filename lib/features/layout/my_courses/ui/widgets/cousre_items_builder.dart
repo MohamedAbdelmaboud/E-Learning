f5dabb1cca88589038f458f83ea8495250f72a07
MohamedAbdelmaboud <mohamedabdlmabod03@gmail.com>
2024-05-25 00:35:01 +0300

    refactor profile

--- lib/features/layout/my_courses/ui/widgets/cousre_items_builder.dart
+++ lib/features/layout/my_courses/ui/widgets/cousre_items_builder.dart
@@ -2,11 +2,30 @@ import 'package:e_learning/common/models/item_model.dart';
 import 'package:e_learning/common/widgets/custom_item_builder.dart';
 import 'package:flutter/material.dart';
 
+import '../../../../../common/utils/assets.dart';
+
 class CourseItemsBuilder extends StatelessWidget {
   const CourseItemsBuilder({
     super.key,
   });
-
+// for course
+  static List<ItemModel> courseItemsModels = [
+    ItemModel(
+      title: 'Lectures',
+      iconPath: Assets.assetsImagesBookColor,
+      trailingText: '50+ Lectures',
+    ),
+    ItemModel(
+      title: 'Learning Time',
+      iconPath: Assets.assetsImagesTimeColor,
+      trailingText: '4 Weeks',
+    ),
+    ItemModel(
+      title: 'Certification',
+      iconPath: Assets.assetsImagesCertificateColor,
+      trailingText: 'Online Certificate',
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return Padding(
